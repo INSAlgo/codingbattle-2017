b21ac4dbbd53d588de6611412d0d862ad973ca3e
Rémy F <yne@users.noreply.github.com>
2017-10-24 10:31:23 +0200

    Update sol-c.c
    Comply with coding rules

--- concours/d_fractale/sol/sol-c.c
+++ concours/d_fractale/sol/sol-c.c
@@ -12,20 +12,20 @@ int pow3(int exp){
 }
 
 char fractal(int x,int y,int layer){
-	for(int i=layer-1;i>=0;i--){
+	for (int i = layer - 1; i >= 0; i--){
 		int p = pow3(i);
-		if((x/p + (y/p)*p)&1)
+		if ((x / p + (y / p) * p) & 1)
 			return ' ';
 	}
 	return 'x';
 }
 
 int main(int argc,char**argv){
-	int layer = getchar()-'0';
+	int layer = getchar() - '0';
 	int size = pow3(layer);
-	for(int y=0;y<size;y++){
-		for(int x=0;x<size;x++)
-			putchar(fractal(x,y,layer));
+	for(int y = 0; y < size; y++){
+		for(int x = 0; x < size; x++)
+			putchar(fractal(x, y, layer));
 		putchar('\n');
 	}
 	return 0;
